685c2462469f4271b74bc490a8a7b2d96f30b7fb
maxingyuan <mxydl2009@163.com>
2024-01-05 19:30:47 +0800

    修复获取文章时传递user的id

--- src/screens/Home/ArticleDetailScreen.jsx
+++ src/screens/Home/ArticleDetailScreen.jsx
@@ -3,13 +3,15 @@ import { useRoute } from '@react-navigation/native';
 import { useState, useEffect } from "react";
 import Article from "../../Components/Article";
 import { requestArticleById } from '../../utils/request';
+import { useSelector } from "react-redux";
 
 function ArticleDetailScreen (props) {
   const [ articleDetail, setArticleDetail ] = useState({});
+  const user = useSelector((state) => state.user);
   const route = useRoute();
   useEffect(() => {
     if (route.params && route.params.articleId) {
-      requestArticleById(route.params.articleId)
+      requestArticleById(route.params.articleId, user._id)
         .then(res => {
           const { article } = res.data;
           console.log('article detail author', article.author)
